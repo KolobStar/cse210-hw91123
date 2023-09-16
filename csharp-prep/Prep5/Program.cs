9d787dc013fafa33b1c4c3efbca031c5d9c51c55
Gloria Estrella <gloriaaguilerasud@gmail.com>
2023-09-16 11:54:57 -0600

    completed

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,54 @@
+//Gloria Estrella
+
 using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
-    }
+        static void Main(string[] args)
+        {
+            
+            DisplayWelcome();//This will call the function
+
+            string userName = PromptUserName();
+            int userNumber = PromptUserNumber();
+
+            int squareNumber = SquareNumber(userNumber);
+
+            DisplayResult(userName, squareNumber);
+
+
+        }
+            static void DisplayWelcome()// this executes what is inside
+            {
+                Console.WriteLine("Welcome to the program! "); 
+            }
+
+            
+            static string PromptUserName()
+            {
+                Console.Write("Please enter your name: ");
+                string name = Console.ReadLine();
+
+                return name;
+            }
+
+
+            static int PromptUserNumber()
+            {
+                Console.Write("Please enter your favorite number: ");
+                int number = int.Parse(Console.ReadLine());
+
+                return number;
+            }
+
+            static int SquareNumber (int number)
+            {
+                int square = number * number;
+                return square;
+            }
+
+            static void DisplayResult(string name, int square)
+            {
+                Console.WriteLine($"{name}, the squre of your number is {square}");
+            }
 }
